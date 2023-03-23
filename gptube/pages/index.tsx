89b7165a57d226a5388cb18452969658ed1cd4bd
saul <luckly083@gmail.com>
2023-03-23 11:11:54 -0500

    Added form to send video to the backend

--- gptube/pages/index.tsx
+++ gptube/pages/index.tsx
@@ -1,26 +1,28 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className="grid grid-cols-2 gap-8 m-6">
-        <div className="space-y-4">
-          <p className="text-4xl text-[#006FFF] text-center">
-            Check what people think about your content
-          </p>
-          <p className="text-xl text-justify">
-            Powered by AI technology, with GPTube you can analyze in a easy way
-            what people think about your social media content in various content
-            platforms such as Youtube, Tiktok, Facebook, Instagram, etc.
-          </p>
-          <div className="flex justify-center">
-            <button className="bg-[#006FFF] hover:bg-blue-700 text-white py-2 px-4 rounded w-48 h-16">
-              <span className="text-lg">Try GPTube</span>
-            </button>
-          </div>
+    <div className="grid grid-cols-2 gap-8 m-6">
+      <div className="space-y-4">
+        <p className="text-4xl text-[#006FFF] text-center">
+          Check what people think about your content
+        </p>
+        <p className="text-xl text-justify">
+          Powered by AI technology, with GPTube you can analyze in a easy way
+          what people think about your social media content in various content
+          platforms such as Youtube, Tiktok, Facebook, Instagram, etc.
+        </p>
+        <div className="flex justify-center">
+          <Link
+            href="/dashboard"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Try GPTube
+          </Link>
         </div>
-        <div className="bg-black opacity-25 rounded-lg"></div>
       </div>
-    </Layout>
+      <div className="bg-black opacity-25 rounded-lg"></div>
+    </div>
   );
 }

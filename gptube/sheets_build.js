a47aef772087b7a0db1bee055c4f8e7de5981c19
saul <luckly083@gmail.com>
2023-04-08 01:58:48 -0500

    changing file extension to .js

---
+++ gptube/sheets_build.js
@@ -0,0 +1,3 @@
+process.env.NODE_OPTIONS=undefined
+delete process.env.NODE_OPTIONS
+process.env.NODE_OPTIONS="--openssl-legacy-provider"
--- gptube/sheets_build.ts
+++
@@ -1,4 +0,0 @@
-process.env.NODE_OPTIONS=undefined
-delete process.env.NODE_OPTIONS
-process.env.NODE_OPTIONS="--openssl-legacy-provider"
-export default {}

2da14cb6095c4ddc5c389c089099237d1d7e0a13
Raymond Negron <raymond1242@Raymonds-MacBook-Air.local>
2023-04-24 23:22:22 -0500

    setting main color

--- gptube/components/Tool/PreAnalysisResult.tsx
+++ gptube/components/Tool/PreAnalysisResult.tsx
@@ -48,23 +48,27 @@ const PreAnalysisResult: React.FC = () => {
             >
                 <div className="flex justify-between">
                     {time === 0 ? (
-                        <p className="flex items-end text-2xl font-medium text-green-600 my-1">
+                        <p className="flex items-end text-2xl font-medium text-typo my-1">
                             Your video have been analyzed!!
                         </p>
                     ) : (
                         <>
-                            <p className="text-white text-lg font-light">
+                            <p className="text-typo text-lg font-light">
                                 Estimated time to complete analyze ...&nbsp;
                                 <span className="text-white text-2xl font-semibold">{time}s</span>
                             </p>
                         </>
                     )}
-                    <Button type="primary" size="large" className="bg-blue-700 text-white p-1 order-last">
-                        {time === 0 ? "Send to email" : "Send email when completed"}
-                    </Button>
+                    <button className="text-base bg-primary border-2 border-primary font-medium text-typo hover:text-primary order-last hover:bg-primary-low rounded-lg px-3 py-2">
+                        {time === 0 ? "Send email" : "Send email when completed"}
+                    </button>
                 </div>
                 <Progress
                     className="my-2"
+                    strokeColor={{
+                        "0%": "#108ee9",
+                        "100%": "#87d068",
+                    }}
                     percent={TIME_OUT / 100 * (TIME_OUT - time)}
                     status={time === 0 ? "success" : "active"}
                     showInfo={false}
@@ -73,16 +77,16 @@ const PreAnalysisResult: React.FC = () => {
             <Content
                 className="bg-black-medium grid mx-auto w-full lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 rounded-2xl shadow-lg md:p-6 sm:py-4 px-4 py-2 items-center"
             >
-                <div className="grid lg:gap-4 gap-2 mx-4">
+                <div className="grid lg:gap-6 gap-2 mx-4">
                     <div className="flex gap-2">
-                        <p className="text-white text-2xl font-medium">Como GitHub Copilot X te Potenciará como Programador (incluye GPT-4)</p>
-                        <p className="text-xl font-medium text-red-600 border-[3px] border-red-600 p-1 h-fit rounded-lg">LIVE</p>
+                        <p className="text-typo text-2xl font-medium">Como GitHub Copilot X te Potenciará como Programador (incluye GPT-4)</p>
+                        {/* <p className="text-xl font-medium text-red-600 border-[3px] border-red-600 p-1 h-fit rounded-lg">LIVE</p> */}
                     </div>
-                    <div className="flex items-center justify-center gap-2 p-2 border-2 border-blue-600 rounded-lg w-fit">
-                        <p className="text-xl sm:text-lg font-normal text-blue-600 px-1">Comments to analyze</p>
-                        <p className="text-2xl font-bold text-blue-600">543.4K</p>
+                    <div className="flex items-center justify-center gap-2 p-2 border-2 border-typo rounded-lg w-fit">
+                        <p className="text-xl sm:text-lg font-normal text-typo px-1">Comments to analyze</p>
+                        <p className="text-2xl font-bold text-typo">543.4K</p>
                     </div>
-                    <div className="flex w-full gap-3">
+                    {/* <div className="flex w-full gap-3">
                         <div className="flex items-center justify-center gap-2 p-2 border-2 border-green-600 rounded-lg w-fit">
                             <LikeTwoTone twoToneColor="#04CB19" style={iconSize} />
                             <p className="text-xl font-semibold text-green-600">53.3K</p>
@@ -91,7 +95,7 @@ const PreAnalysisResult: React.FC = () => {
                             <DislikeTwoTone twoToneColor="#F10505" style={iconSize} />
                             <p className="text-xl font-semibold text-red-600">2343</p>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="flex-col gap-1">
                         {tags.map((tag: string, index: number) => {
                             return index < 8 ? (
--- gptube/tailwind.config.js
+++ gptube/tailwind.config.js
@@ -12,8 +12,17 @@ module.exports = {
     extend: {
       colors: {
         "black-full": "#1A1A1A",
-        "black-medium": "#2D2D2D",
+        "black-medium": "#3A4750",
         "black-low": "#494949",
+        "primary": "#E84545",
+        "primary-low": "#FFEBEB",
+        "typo": "#F2F2F2",
+        // "black-full": "#0A2647",
+        // "black-medium": "#144272",
+        // "black-low": "#205295",
+        // "black-full": "#222831",
+        // "black-medium": "#2C394B",
+        // "black-low": "#FF4C29",
       }
     },
   },

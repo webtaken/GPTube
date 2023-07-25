a1e828557dcab6b2238fba798614fecfbf7ed5f4
saul <luckly083@gmail.com>
2023-07-25 16:57:53 -0500

    Verification from lemonSqueezy is set on a new function

--- gptube/pages/api/subscription_webhook.ts
+++ gptube/pages/api/subscription_webhook.ts
@@ -4,41 +4,44 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const webhookPass = process.env.LEMON_WEBHOOK_PASS || "";
 
+const verifySignature = (req: NextApiRequest) => {
+  const secret = webhookPass;
+  const hmac = crypto.createHmac("sha256", secret);
+  const requestBody = Array.isArray(req.body)
+    ? req.body.join("")
+    : JSON.stringify(req.body);
+
+  const signature = req.headers["x-signature"];
+  if (!signature || typeof signature !== "string") {
+    throw new Error("Invalid signature.");
+  }
+
+  const digest = Buffer.from(hmac.update(requestBody).digest("hex"), "hex");
+  const signatureBuffer = Buffer.from(signature, "hex");
+
+  if (!crypto.timingSafeEqual(digest, signatureBuffer)) {
+    throw new Error("Invalid signature.");
+  }
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
     try {
-      const secret = webhookPass;
-      const hmac = crypto.createHmac("sha256", secret);
-      const requestBody = Array.isArray(req.body)
-        ? req.body.join("")
-        : JSON.stringify(req.body);
-
-      // Access the X-Signature header directly from the `req.headers` object
-      const signature = req.headers["x-signature"];
-      if (!signature || typeof signature !== "string") {
-        throw new Error("Invalid signature.");
-      }
-
-      const digest = Buffer.from(
-        hmac.update(requestBody).digest("hex"),
-        "utf8"
-      );
-      const signatureBuffer = Buffer.from(signature, "utf8");
-
-      if (!crypto.timingSafeEqual(digest, signatureBuffer)) {
-        console.log("Invalid signature");
-        throw new Error("Invalid signature.");
-      }
+      verifySignature(req);
       console.log(req.body);
-
       res.status(200);
     } catch (error) {
       console.log(error);
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+        return;
+      }
       res.status(500).json({ error });
     }
+    return;
   }
   res.status(403);
 }

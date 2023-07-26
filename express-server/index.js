f2bf8e0bd8d86fe30a89c843c13b28e52231e8ea
saul <luckly083@gmail.com>
2023-07-26 10:18:21 -0500

    Solving request

--- express-server/index.js
+++ express-server/index.js
@@ -20,7 +20,7 @@ app.post("/subscriptions", async (req, res) => {
   try {
     const hmac = crypto.createHmac("sha256", secret);
     const digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
-    const signature = Buffer.from(request.get("X-Signature") || "", "utf8");
+    const signature = Buffer.from(req.get("X-Signature") || "", "utf8");
 
     if (!crypto.timingSafeEqual(digest, signature)) {
       throw new Error("Invalid signature.");

3c2e0bc8876c07ed6b457660b731a45c016da986
saul <luckly083@gmail.com>
2023-07-26 10:07:27 -0500

    Debugging subscriptions

--- express-server/index.js
+++ express-server/index.js
@@ -27,6 +27,7 @@ app.post(
       const body = JSON.parse(req.body);
       res.status(200).json({ ...body });
     } catch (error) {
+      console.log(error);
       res.status(500).json({ ...error });
     }
   }

9f5bab2edf0e270b101466a4579b54d56d8ff738
saul <luckly083@gmail.com>
2023-07-26 10:15:24 -0500

    adding middlewares

--- express-server/index.js
+++ express-server/index.js
@@ -5,34 +5,34 @@ const app = express();
 const port = process.env.PORT || 8080;
 const secret = process.env.LEMON_WEBHOOK_PASS || "";
 
+// Custom middleware to store the raw body in the request object
+app.use(express.raw({ type: "application/json" }));
+app.use((req, _res, next) => {
+  req.rawBody = req.body;
+  next();
+});
+
 app.get("/", (_req, res) => {
   return res.send("GPTUBE-subscriptions server");
 });
 
-app.post(
-  "/subscriptions",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    try {
-      const hmac = crypto.createHmac("sha256", secret);
-      const digest = Buffer.from(
-        hmac.update(req.rawBody).digest("hex"),
-        "utf8"
-      );
-      const signature = Buffer.from(request.get("X-Signature") || "", "utf8");
+app.post("/subscriptions", async (req, res) => {
+  try {
+    const hmac = crypto.createHmac("sha256", secret);
+    const digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
+    const signature = Buffer.from(request.get("X-Signature") || "", "utf8");
 
-      if (!crypto.timingSafeEqual(digest, signature)) {
-        throw new Error("Invalid signature.");
-      }
-
-      const body = JSON.parse(req.body);
-      res.status(200).json({ ...body });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ ...error });
+    if (!crypto.timingSafeEqual(digest, signature)) {
+      throw new Error("Invalid signature.");
     }
+
+    const body = JSON.parse(req.body);
+    res.status(200).json({ ...body });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ ...error });
   }
-);
+});
 
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);

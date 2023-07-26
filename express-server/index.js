5f5474790e75b773fc01e32e1d38effa17fb3253
saul <luckly083@gmail.com>
2023-07-26 09:47:41 -0500

    Adding webhook functionality for lemon squeezy

--- express-server/index.js
+++ express-server/index.js
@@ -1,14 +1,28 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const secret = process.env.LEMON_WEBHOOK_PASS || "";
 
 app.get("/", (_req, res) => {
-  return res.send("Express on Vercel");
+  return res.send("GPTUBE-subscriptions server");
 });
 
-app.get("/ping", (_req, res) => {
-  return res.send("pong 🏓");
-});
+app.get(
+  "/subscriptions",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const hmac = crypto.createHmac("sha256", secret);
+    const digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
+    const signature = Buffer.from(request.get("X-Signature") || "", "utf8");
+
+    if (!crypto.timingSafeEqual(digest, signature)) {
+      throw new Error("Invalid signature.");
+    }
+    const body = JSON.parse(req.body);
+    res.json({ ...body });
+  }
+);
 
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);

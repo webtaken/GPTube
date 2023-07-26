a2131cce64c53c0f83ee5ba71060c7ae1fc999e9
saul <luckly083@gmail.com>
2023-07-25 23:15:22 -0500

    Adding solution with disabling body parsing

--- gptube/pages/api/subscription_webhook.ts
+++ gptube/pages/api/subscription_webhook.ts
@@ -1,23 +1,68 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
 import crypto from "crypto";
-import getRawBody from "raw-body";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const webhookPass = process.env.LEMON_WEBHOOK_PASS || "";
 
-const verifySignature = async (req: NextApiRequest) => {
-  const secret = webhookPass;
-  const hmac = crypto.createHmac("sha256", secret);
-  const requestBody = await getRawBody(req.body);
-  const signature = req.headers["x-signature"] || "";
-  if (typeof signature !== "string") {
-    throw new Error("Invalid signature.");
-  }
-  const digest = Buffer.from(hmac.update(requestBody).digest("hex"), "utf-8");
-  const signatureBuffer = Buffer.from(signature, "utf-8");
+// const verifySignature = async (req: NextApiRequest) => {
+//   const secret = webhookPass;
+//   const hmac = crypto.createHmac("sha256", secret);
+//   const requestBody = await getRawBody(req.body);
+//   const signature = req.headers["x-signature"] || "";
+//   if (typeof signature !== "string") {
+//     throw new Error("Invalid signature.");
+//   }
+//   const digest = Buffer.from(hmac.update(requestBody).digest("hex"), "utf-8");
+//   const signatureBuffer = Buffer.from(signature, "utf-8");
 
-  if (!crypto.timingSafeEqual(digest, signatureBuffer)) {
-    throw new Error("Invalid signature.");
-  }
+//   if (!crypto.timingSafeEqual(digest, signatureBuffer)) {
+//     throw new Error("Invalid signature.");
+//   }
+// };
+
+const verifySignature = (req: NextApiRequest) => {
+  return new Promise<void>((resolve, reject) => {
+    const secret = webhookPass;
+    const hmac = crypto.createHmac("sha256", secret);
+
+    const chunks: any[] = [];
+    let requestBodySize = 0;
+
+    req.on("data", (chunk) => {
+      chunks.push(chunk);
+      requestBodySize += chunk.length;
+    });
+
+    req.on("end", () => {
+      const requestBody = Buffer.concat(chunks, requestBodySize);
+      const signature = String(req.headers["x-signature"] || "");
+
+      if (typeof signature !== "string") {
+        reject(new Error("Invalid signature."));
+      }
+
+      const digest = Buffer.from(hmac.update(requestBody).digest("hex"), "utf-8");
+      const signatureBuffer = Buffer.from(signature, "utf-8");
+
+      console.log(digest.toString());
+      console.log(requestBody.toString());
+
+      if (!crypto.timingSafeEqual(digest, signatureBuffer)) {
+        reject(new Error("Invalid signature."));
+      }
+
+      resolve();
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+    });
+  });
 };
 
 export default async function handler(

ef441183138cb024224f445b25589ac5dfccb576
Camilo Mora <85380672+CamiloEMP@users.noreply.github.com>
2023-08-17 22:06:48 -0500

    Apply Eslint

--- gptube/components/UI/Tooltip.tsx
+++ gptube/components/UI/Tooltip.tsx
@@ -1,47 +1,44 @@
-import React from "react";
+import React from 'react'
 
 interface TooltipProps {
-  children: React.ReactNode;
-  title: string;
-  position?: "top" | "right" | "left" | "bottom";
+  children: React.ReactNode
+  title: string
+  position?: 'top' | 'right' | 'left' | 'bottom'
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ children, title, position }) => {
-  return (
-    <div className="group relative inline-block">
-      {children}
-      {position === "right" && (
-        <div className="absolute left-full top-1/2 z-20 -translate-y-1/2 whitespace-nowrap rounded bg-black py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100">
-          <span className="absolute left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45 rounded-sm bg-black"></span>
-          {title}
-        </div>
-      )}
-      {position === "left" && (
-        <div className="absolute right-full top-1/2 z-20 -translate-y-1/2 whitespace-nowrap rounded bg-black py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100">
-          <span className="absolute right-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45 rounded-sm bg-black"></span>
-          {title}
-        </div>
-      )}
-      {position === "bottom" && (
-        <div className="absolute top-full left-1/2 z-20 -translate-x-1/2 whitespace-nowrap rounded bg-black py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100">
-          <span className="absolute top-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm bg-black"></span>
-          {title}
-        </div>
-      )}
-      {position === "top" && (
-        <div className="absolute bottom-full left-1/2 z-20 -translate-x-1/2 whitespace-nowrap rounded bg-black py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100">
-          <span className="absolute bottom-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm bg-black"></span>
-          {title}
-        </div>
-      )}
-      {position === undefined && (
-        <div className="absolute bottom-full left-1/2 z-20 -translate-x-1/2 whitespace-nowrap rounded bg-black py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100">
-          <span className="absolute bottom-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm bg-black"></span>
-          {title}
-        </div>
-      )}
-    </div>
-  );
-};
+const variants = {
+  top: {
+    container: 'left-1/2 -translate-x-1/2 bottom-full',
+    item: 'bottom-[-3px] left-1/2 -translate-x-1/2',
+  },
+  bottom: {
+    container: 'top-full left-1/2 -translate-x-1/2',
+    item: 'top-[-3px] left-1/2 -translate-x-1/2',
+  },
+  right: {
+    container: 'left-full top-1/2 -translate-y-1/2',
+    item: 'left-[-3px] top-1/2 -translate-y-1/2',
+  },
+  left: {
+    container: 'right-full top-1/2 -translate-y-1/2',
+    item: 'right-[-3px] top-1/2 -translate-y-1/2',
+  },
+}
 
-export default Tooltip;
+function Tooltip({ children, title, position = 'top' }: TooltipProps) {
+  return (
+    <div className="relative inline-block group">
+      {children}
+      <div
+        className={`absolute z-20 whitespace-nowrap rounded bg-black py-[6px] px-4 text-sm font-semibold text-white opacity-0 group-hover:opacity-100 ${variants[position].container}`}
+      >
+        <span
+          className={`absolute w-2 h-2 rotate-45 bg-black rounded-sm -z-10 ${variants[position].item}`}
+        />
+        {title}
+      </div>
+    </div>
+  )
+}
+
+export default Tooltip
--- gptube/components/UI/YoutubeEmbed.tsx
+++ gptube/components/UI/YoutubeEmbed.tsx
@@ -1,20 +1,23 @@
-import React from "react";
-import styles from "./YoutubeEmbed.module.css";
+import React from 'react'
+
+import styles from './YoutubeEmbed.module.css'
 
 interface YoutubeEmbedProps {
-  title: string;
-  embedId: string;
+  title: string
+  embedId: string
 }
 
-const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ title, embedId }) => (
-  <div className={styles["video-responsive"]}>
-    <iframe
-      src={`https://www.youtube.com/embed/${embedId}`}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title={title}
-    />
-  </div>
-);
+function YoutubeEmbed({ title, embedId }: YoutubeEmbedProps) {
+  return (
+    <div className={styles['video-responsive']}>
+      <iframe
+        allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        title={title}
+      />
+    </div>
+  )
+}
 
-export default YoutubeEmbed;
+export default YoutubeEmbed

0f7090a3e74c7cebfbc874e2b4b4a772641d1d8b
Camilo Mora <85380672+CamiloEMP@users.noreply.github.com>
2023-10-11 16:29:17 -0500

    Fix protected route component

--- gptube/components/Security/ProtectedRoute.tsx
+++ gptube/components/Security/ProtectedRoute.tsx
@@ -2,24 +2,19 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 import { useAuth } from '@/hooks/useAuth'
-import { localStoreAuthVar } from '@/context/AuthContext/AuthContext'
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { user } = useAuth()
   const router = useRouter()
 
   useEffect(() => {
-    const isAuthenticated = localStorage.getItem(localStoreAuthVar)
-
-    if (!user && !isAuthenticated) {
+    if (!user) {
       router.push({
         pathname: '/login',
         query: {
           from: router.query.from,
         },
       })
-    } else {
-      localStorage.setItem(localStoreAuthVar, 'true')
     }
   }, [router, user])
 

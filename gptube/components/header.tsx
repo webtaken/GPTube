e999b16452102e2e47e664f78941c6411ca08999
Camilo Mora <85380672+CamiloEMP@users.noreply.github.com>
2023-10-12 16:41:30 -0500

    Add a way to logout, (debug purpose)

--- gptube/components/header.tsx
+++ gptube/components/header.tsx
@@ -1,20 +1,33 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar'
 import Link from 'next/link'
 
+import { useAuth, useAuthActions } from '@/hooks/use-auth'
+
 import { Button } from './Common/button'
 
 export function Header() {
+  const { user } = useAuth()
+  const { logoutHandler } = useAuthActions()
+
   return (
     <Navbar isBordered maxWidth="lg">
       <NavbarBrand>
         <p className="text-2xl font-bold">GPTube</p>
       </NavbarBrand>
       <NavbarContent justify="end">
-        <NavbarItem>
-          <Button as={Link} color="success" href="/login" radius="md" variant="flat">
-            Sign in
-          </Button>
-        </NavbarItem>
+        {user ? (
+          <NavbarItem>
+            <Button color="danger" radius="md" variant="flat" onClick={logoutHandler}>
+              Sign out
+            </Button>
+          </NavbarItem>
+        ) : (
+          <NavbarItem>
+            <Button as={Link} color="success" href="/login" radius="md" variant="flat">
+              Sign in
+            </Button>
+          </NavbarItem>
+        )}
       </NavbarContent>
     </Navbar>
   )
